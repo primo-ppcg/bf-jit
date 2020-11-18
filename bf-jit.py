755b64667701a79c66f72c8c73a22c15392e3217
primo-ppcg <mike.tryczak@gmail.com>
2020-11-19 00:10:40 +0700

    Add `-c` command line option

--- bf-jit.py
+++ bf-jit.py
@@ -1,9 +1,13 @@
 import os
 from rpython.rlib.jit import JitDriver
+from rpython.rlib.rarithmetic import build_int, widen
+
+r_uint16 = build_int('r_short', False, 16)
 
 jitdriver = JitDriver(
   greens = ['pc', 'proglen', 'program'],
-  reds   = ['pointer', 'pointer_rel', 'tape'])
+  reds   = 'auto'
+)
 
 # one byte instructions
 MV, SHFT, PUTC, GETC = 0x00, 0x20, 0x40, 0x60
@@ -12,23 +16,21 @@ ADD, MUL = 0x80, 0xA0
 # three byte instructions
 JRZ, JRNZ = 0xC0, 0xE0
 
+
 def mainloop(program):
   pc = 0
   proglen = len(program)
   tape = bytearray("\0"*65536)
-  pointer = 0
-  pointer_rel = 0
+  pointer = r_uint16(0)
+  pointer_rel = r_uint16(0)
 
   while pc < proglen:
-    jitdriver.jit_merge_point(
-      pc=pc, proglen=proglen, program=program,
-      pointer=pointer, pointer_rel=pointer_rel, tape=tape
-    )
+    jitdriver.jit_merge_point(pc=pc, proglen=proglen, program=program)
 
     code = ord(program[pc])
     # 5-bit signed shift
     shift = (code & 0x0F) - (code & 0x10)
-    pointer_rel = pointer_rel + shift & 65535
+    pointer_rel = r_uint16(widen(pointer_rel) + shift)
     command = code & 0xE0
 
     if command == ADD:
@@ -74,7 +76,8 @@ def mainloop(program):
       pc += 1
 
 
-def parse(program, i = 0):
+def parse(program, i = 0, depth = 0):
+  # TODO: unwrap balanced loops without MV as MUL
   parsed = bytes()
   proglen = len(program)
   shift = 0
@@ -94,12 +97,12 @@ def parse(program, i = 0):
           parsed += chr(SHFT | 0x10)
           shift += 16
       elif char == '[':
-        if program[i + 1] in '-+' and program[i + 2] == ']':
+        if program[i + 1] in '+-' and program[i + 2] == ']':
           parsed += chr(MV | (shift & 0x1F)) + chr(MUL) + chr(255)
           shift = 0
           i += 2
         else:
-          subprog, i = parse(program, i + 1)
+          subprog, i, _ = parse(program, i + 1, depth + 1)
           sublen = len(subprog)
           jump = chr((sublen + 3) >> 8) + chr((sublen + 3) & 0xFF)
           parsed += chr(JRZ | (shift & 0x1F)) + jump + subprog
@@ -107,7 +110,7 @@ def parse(program, i = 0):
       elif char == ']':
         sublen = len(parsed)
         jump = chr(sublen >> 8) + chr(sublen & 0xFF)
-        return parsed + chr(JRNZ | (shift & 0x1F)) + jump, i
+        return parsed + chr(JRNZ | (shift & 0x1F)) + jump, i, depth - 1
       elif char == '.':
         parsed += chr(PUTC | (shift & 0x1F))
         shift = 0
@@ -123,37 +126,66 @@ def parse(program, i = 0):
         shift = 0
     i += 1
 
-  return parsed, i
+  return parsed, i, depth
 
-def run(fp):
-  program_contents = ''
-  while True:
-    read = os.read(fp, 4096)
-    if len(read) == 0:
-      break
-    program_contents += read
-  os.close(fp)
-  program, i = parse(program_contents)
-  mainloop(program)
 
 def main(argv):
-  filename = ''
+  from rgetopt import gnu_getopt, GetoptError
+
   try:
-    filename = argv[1]
-    fp = os.open(filename, os.O_RDONLY, 0777)
-  except IndexError:
-    os.write(2, 'Usage: %s program.bf'%argv[0])
-    return 1
-  except OSError:
-    os.write(2, 'File not found: %s'%filename)
+    optlist, args = gnu_getopt(argv[1:], 'hc:', ['help', 'code='])
+  except GetoptError as ex:
+    os.write(2, ex.msg + '\n')
     return 1
 
-  run(fp)
+  source = ''
+  for opt, val in optlist:
+    if opt == '-c' or opt == '--code':
+      source = val
+    elif opt == '-h' or opt == '--help':
+      display_usage(argv[0])
+      display_help()
+      return 1
+
+  if source == '':
+    try:
+      with open(args[0]) as file:
+        source = file.read()
+    except IndexError:
+      display_usage(argv[0])
+      return 1
+    except IOError:
+      os.write(2, 'File not found: %s\n'%args[0])
+      return 1
+
+  program, i, depth = parse(source)
+  if depth > 0:
+    os.write(2, 'Unmatched `[` in source\n')
+    return 1
+  elif depth < 0:
+    os.write(2, 'Unmatched `]` in source\n')
+    return 1
+  mainloop(program)
+
   return 0
+
+
+def display_usage(name):
+  os.write(2, 'Usage: %s [-h] (<file> | -c <code>)\n'%name)
+
+def display_help():
+  os.write(2, '''
+A just-in-time compiling interpreter for the brainfuck programming language.
+
+Arguments:
+  file          a brainfuck script file to execute
+
+Options:
+  -c, --code=   a string of instructions to be executed
+                if present, the file argument will be ignored
+  -h, --help    display this message
+''')
+
 
 def target(*args):
   return main
-
-if __name__ == '__main__':
-  import sys
-  main(sys.argv)
